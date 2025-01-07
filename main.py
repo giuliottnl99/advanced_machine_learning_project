afa9682eabdc1583cc33f2a91ddd5d349a92c15f
Giulio Titonel <giuliorossi11@yahoo.it>
2025-01-07 16:42:40 +0100

    LR with cosine annealing, commented V2 model to delete, changed printed output

--- main.py
+++ main.py
@@ -12,56 +12,58 @@ from torch.utils.data import random_split
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import torch_optimizer as optim
+import torch_optimizer as optim2
+import math
 
 
-class LeNet5V2(nn.Module):
-    def __init__(self, args, num_classes=100):
-        super(LeNet5V2, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+#for now I comment, but we are not allowed to use this architecture!
+# class LeNet5V2(nn.Module):
+#     def __init__(self, args, num_classes=100):
+#         super(LeNet5V2, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2)
+#         self.bn1 = nn.BatchNorm2d(64)
+#         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2)  # Added padding
-        self.bn2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+#         self.conv2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2)  # Added padding
+#         self.bn2 = nn.BatchNorm2d(64)
+#         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # Adjusted size calculation based on padding
-        self.fc1 = nn.Linear(64 * 8 * 8, 384)  # Updated size
-        self.bn3 = nn.BatchNorm1d(384)
-        self.dropout1 = nn.Dropout(args.dropout1)
+#         # Adjusted size calculation based on padding
+#         self.fc1 = nn.Linear(64 * 8 * 8, 384)  # Updated size
+#         self.bn3 = nn.BatchNorm1d(384)
+#         self.dropout1 = nn.Dropout(args.dropout1)
 
-        self.fc2 = nn.Linear(384, 192)
-        self.bn4 = nn.BatchNorm1d(192)
-        self.dropout2 = nn.Dropout(args.dropout2)
+#         self.fc2 = nn.Linear(384, 192)
+#         self.bn4 = nn.BatchNorm1d(192)
+#         self.dropout2 = nn.Dropout(args.dropout2)
 
-        self.fc3 = nn.Linear(192, num_classes)
+#         self.fc3 = nn.Linear(192, num_classes)
 
-        # Proper initialization
-        if args.init_weights:
-            self._initialize_weights()
+#         # Proper initialization
+#         if args.init_weights:
+#             self._initialize_weights()
 
-    def forward(self, x):
-        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 64 * 8 * 8)  # Updated size
-        x = self.dropout1(F.relu(self.bn3(self.fc1(x))))
-        x = self.dropout2(F.relu(self.bn4(self.fc2(x))))
-        x = self.fc3(x)
-        return x
+#     def forward(self, x):
+#         x = self.pool1(F.relu(self.bn1(self.conv1(x))))
+#         x = self.pool2(F.relu(self.bn2(self.conv2(x))))
+#         x = x.view(-1, 64 * 8 * 8)  # Updated size
+#         x = self.dropout1(F.relu(self.bn3(self.fc1(x))))
+#         x = self.dropout2(F.relu(self.bn4(self.fc2(x))))
+#         x = self.fc3(x)
+#         return x
 
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                nn.init.constant_(m.bias, 0)
+#     def _initialize_weights(self):
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+#                 if m.bias is not None:
+#                     nn.init.constant_(m.bias, 0)
+#             elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
+#                 nn.init.constant_(m.weight, 1)
+#                 nn.init.constant_(m.bias, 0)
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+#                 nn.init.constant_(m.bias, 0)
 
 
 class LeNet5(nn.Module):
@@ -117,9 +119,9 @@ def create_optimizer(opt_name, model, lr, weight_decay=0.0001):
     elif opt_name.lower() == 'adam':
         return optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     elif opt_name.lower() == 'lars':
-        return optim.LARS(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=0.9, eps=1e-8, trust_coef=0.001)
+        return optim2.LARS(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=0.9, eps=1e-8, trust_coefficient=0.001)
     elif opt_name.lower() == 'lamb':
-        return optim.LAMB(model.parameters(), lr=lr, weight_decay=weight_decay, betas=(0.9, 0.999), eps=1e-6)
+        return optim2.LAMB(model.parameters(), lr=lr, weight_decay=weight_decay, betas=(0.9, 0.999), eps=1e-6)
     else:
         raise ValueError(f"Optimizer {opt_name} not supported")
 
@@ -232,12 +234,14 @@ def train(args):
 
     # Training loop
     for epoch in range(args.start_epoch, args.epochs):
+        #change lr by applying cosine annealing:
+        lr = args.lr_min + 0.5 * (args.lr_max - args.lr_min) * (1 + math.cos(math.pi * epoch / args.epochs))
+
         model.train()
         local_losses = []
 
         for i, local_model in enumerate(local_models):
-            local_optimizer = create_optimizer(args.optimizer, local_model, args.learning_rate)
-            local_loss = 0.0
+            local_optimizer = create_optimizer(args.optimizer, local_model, lr)
 
             if is_avg_epoch:
                 local_model.load_state_dict(model.state_dict())
@@ -278,13 +282,12 @@ def train(args):
             torch.save(model.state_dict(), args.best_model)
             print(f"Best model saved with loss = {best_loss}")
 
-        print(f"Epoch {epoch+1}: Loss = {epoch_train_loss}")
+        print(f"Epoch {epoch+1}: Training Loss = {epoch_train_loss}")
 
         # Evaluate on test set periodically
         if (epoch + 1) % args.eval_interval == 0:
             test_acc = evaluate(model, test_loader, device)
             train_acc = evaluate(model, train_loader, device)
-            print(f"Epoch {epoch+1} - Test Accuracy: {test_acc:.2f}% - Train Accuracy: {train_acc:.2f}%")
             epochs.append(epoch+1)
             trainAccuracies.append(train_acc)
             testAccuracies.append(test_acc)
@@ -292,6 +295,7 @@ def train(args):
             train_losses.append(epoch_train_loss) #TODO: verify if this this works. Before it was just loss.item(). But avg may be more stable
             test_loss = compute_test_loss(model, test_loader, criterion, device)
             test_losses.append(test_loss)
+            print(f"Epoch {epoch+1} - Test Accuracy: {test_acc:.2f}% - Train Accuracy: {train_acc:.2f}% - Test Loss: {test_loss} - Current LR: {lr}")
             with open('accuraciesAndLosses.csv', mode='a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow([epoch+1, train_acc, test_acc, epoch_train_loss, test_loss])  # Write headers
@@ -309,7 +313,8 @@ def main():
     parser.add_argument('--J', type=int, default=1, help='Interval for weight averaging')
     parser.add_argument('--epochs', type=int, default=150, help='Number of training epochs')
     parser.add_argument('--batch-size', type=int, default=64, help='Batch size for training')
-    parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning rate')
+    parser.add_argument('--lr-max', type=float, default=0.01, help='Learning rate max of cosine annealing')
+    parser.add_argument('--lr-min', type=float, default=0.001, help='Learning rate min of cosine annealing')
     parser.add_argument('--weight-decay', type=float, default=0.0001, help='Weight decay')
     parser.add_argument('--optimizer', type=str, default='sgd',
                        choices=['sgd', 'adam', 'lars', 'lamb'], help='Optimizer to use')
@@ -319,21 +324,22 @@ def main():
                        help='Path to save last model')
     parser.add_argument('--starting-model', type=str, default=None,
                        help='Path to starting model (optional)')
-    parser.add_argument('--start-epoch', type=str, default=0,
+    parser.add_argument('--start-epoch', type=int, default=0,
                        help='If you started with a trained model, set the epoch the process got stuck')
 
     parser.add_argument('--eval-interval', type=int, default=5,
                        help='Interval for evaluation during training')
-    parser.add_argument('--transform', type=bool, default=False,
+    parser.add_argument('--transform', type=bool, default=True,
                        help='Say if we want to add harder transformations or not')
     parser.add_argument('--model-version', type=int, default=1,
                        help='Say what version of the model we want to use (1 or 2)')
-    parser.add_argument('--dropout1', type=float, default=0,
-                       help='Says the first dropout. If 0 it is not applied. 0.5 is a default.')
-    parser.add_argument('--dropout2', type=float, default=0,
-                       help='Says the second dropout. If 0 it is not applied. 0.5 is a default.')
-    parser.add_argument('--init-weights', type=bool, default=False,
-                       help='Says if weights must be initialized or not.')
+    #must be removed: model 2 is not authorized!
+    # parser.add_argument('--dropout1', type=float, default=0,
+    #                    help='Says the first dropout. If 0 it is not applied. 0.5 is a default.')
+    # parser.add_argument('--dropout2', type=float, default=0,
+    #                    help='Says the second dropout. If 0 it is not applied. 0.5 is a default.')
+    # parser.add_argument('--init-weights', type=bool, default=False,
+    #                    help='Says if weights must be initialized or not.')
 
     args, uknown = parser.parse_known_args()
 
